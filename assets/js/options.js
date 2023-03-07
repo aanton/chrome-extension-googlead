71efdf962d820ab9c3b3bafb7f8f9c8d523c590d
aanton <aanton@gmail.com>
2023-03-07 11:59:33 +0100

    New options page to set the GAM Network id

---
+++ assets/js/options.js
@@ -0,0 +1,37 @@
+// This script is used in the Options page
+
+const networkIdElement = document.getElementById('networkId');
+const statusElement = document.getElementById('status');
+
+const saveOptions = function(e) {
+  e.preventDefault();
+
+  chrome.storage.sync.set(
+    {
+      networkId: networkIdElement.value,
+    },
+    () => {
+      statusElement.textContent = 'Options saved';
+      statusElement.style.display = 'block';
+
+      setTimeout(() => {
+        statusElement.textContent = '';
+        statusElement.style.display = 'none';
+      }, 1000);
+    }
+  );
+}
+
+const restoreOptions = function(e) {
+  chrome.storage.sync.get(
+    {
+      networkId: '',
+    },
+    (data) => {
+      networkIdElement.value = data.networkId;
+    }
+  );
+}
+
+document.addEventListener('DOMContentLoaded', restoreOptions);
+document.getElementById('save').addEventListener('click', saveOptions);
