036005a64b8ec604a14387014c435a73c5036571
aanton <aanton@gmail.com>
2024-09-12 23:45:39 +0200

    Filter requests & slots by adunit name

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -7,3 +7,15 @@ export const readHeader = function(header, name) {
   const found = header.find(parameter => parameter.name === name);
   return found ? found.value : false;
 };
+
+export const wait = function (ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+};
+
+export const debounce = function (callback, wait) {
+  let timeout;
+  return (...args) => {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => callback(...args), wait);
+  };
+};
